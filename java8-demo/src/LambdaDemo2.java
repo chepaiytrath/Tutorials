024dd55c5d514c59318cff3b242d5190dc92c8c3
Jatin Shashoo <jatin.shashoo@gmail.com>
2018-06-18 13:17:10 +0530

    Changes in LambdaDemo2

--- java8-demo/src/LambdaDemo2.java
+++ java8-demo/src/LambdaDemo2.java
@@ -64,6 +64,61 @@ public class LambdaDemo2 {
 		// If you want an API with multiple args, just create one by extending the ones provided
 		
 		
+		// Consumer contract needs one argument at least. If none/multiple exist : incompatible type error
+		// ERROR : Lambda expression's signature does not match the signature of the functional interface method accept(Object)
+		Consumer expr10 = (a, b) -> {
+			System.out.println("Hello ");
+		}; 
+		
+		Consumer expr11 = () -> {
+			System.out.println("Hello ");
+		}; 
+		
+		// Can use some old interfaces as Functional Interfaces. Ex. Runnable
+		Runnable expr12 = () -> {
+			System.out.println("Hello ");
+		};
+		
+		// Single line method definition can be defined without {}
+		Consumer<String> expr13 = (name) -> System.out.println("Hello " + name);
+		
+		// Can remove () from argument level
+		Consumer<String> expr14 = name -> System.out.println("Hello " + name);
+		
+		// Generic datatype is return datatype
+		Supplier<Integer> expr15 = () -> {
+			System.out.println("Calculating");
+			return 10+20;
+		};
+		
+		// Without generic datatype
+		Supplier expr16 = () -> {
+			System.out.println("Calculating");
+			return 10+20;
+		};
+		
+		// One LOC
+		Supplier expr17 = () -> { return 10+20;	};
+		
+		// RETURN keyword is optional BUT remove curly braces.
+		//Supplier expr18 = () ->  return 10+20;
+		
+		// Without return and ()
+		Supplier expr19 = () ->  10+20;
+		
+		// Supplier generic datatype is for return type
+		Supplier<Integer> expr20 = (Integer a) -> {
+			System.out.println("Calculating");
+			return 10+20;
+		};
+		
+		// Use Function to cater above
+		Function<Integer, Integer> expr21 = (Integer a) -> {
+			System.out.println("Calculating");
+			return 10+20;
+		};
+		
+		// In case requirement is for multiple args, just create your own FI and use.
 	}
 	
 }
